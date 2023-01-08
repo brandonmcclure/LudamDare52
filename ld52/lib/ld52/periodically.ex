9f024d84d44f0615c9f1d4a0aa00892c2a0ae28b
Brandon McClure <brandonmcclure89@gmail.com>
2023-01-08 09:28:50 -0700

    Setup pubsub so the live view will update when an game hour passes

--- ld52/lib/ld52/periodically.ex
+++ ld52/lib/ld52/periodically.ex
@@ -2,6 +2,7 @@ defmodule Ld52.Periodically do
   use GenServer
   alias Ld52.{Repo,ServerState}
   import Ecto.Query, only: [from: 2]
+  alias Phoenix.PubSub
 
   def start_link(_opts) do
     GenServer.start_link(__MODULE__, %{})
@@ -14,24 +15,23 @@ defmodule Ld52.Periodically do
     found != nil
   end
   def init(state) do
-
     schedule_work() # Schedule work to be performed at some point
     if(!server_state_by_id(1)) do
-      dbg("init here")
-      case Repo.insert(%ServerState{id: 1, hourssincecreation: 0, realsecondspergamehour: 10}) do
+      #dbg("init here")
+      case Repo.insert(%ServerState{id: 1, hourssincecreation: 0, realsecondspergamehour: 1}) do
 
         {:ok, server_state} ->
-          dbg("ok")
+          #dbg("ok")
           state = %{serverstate: server_state}
 
         {:error, _changeset} ->
-          dbg("error")
+          #dbg("error")
           state = %{serverstate: state}
       end
     else
-      dbg("Passing on existing state")
+      #dbg("Passing on existing state")
       state = %{serverstate: Repo.get!(ServerState, 1)}
-      dbg(state)
+      #dbg(state)
       {:ok, state}
     end
 
@@ -39,23 +39,24 @@ defmodule Ld52.Periodically do
   end
 
   def handle_info(:work, state) do
-
+    state = %{serverstate: Repo.get!(ServerState, 1)}
+    ((Repo.get!(ServerState, 1)).realsecondspergamehour) * 1000
     dbg("time keeps on sliping")
     dbg(state)
     hr = state.serverstate.hourssincecreation
-    dbg(hr)
+    #dbg(hr)
     new_state =
       Ecto.Changeset.change(state.serverstate, %{
         hourssincecreation: hr + 1
       })
 
       state = Repo.update!(new_state)
-
+      PubSub.broadcast(Ld52.PubSub,"serverstate",state)
     schedule_work() # Reschedule once more
     {:noreply, state}
   end
 
   defp schedule_work() do
-    Process.send_after(self(), :work, ((Repo.get!(ServerState, 1)).realsecondspergamehour) * 1000)
+    Process.send_after(self(), :work, (10 * 1000))
   end
 end
